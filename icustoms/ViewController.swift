e4c0b9e406d8cf026a97621fb35db60700422d9d
Кузин Дмитрий <kuzin_d@tass.ru>
2019-03-21 18:27:48 +0300

    Initial Commit

---
+++ icustoms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  icustoms
+//
+//  Created by Dmitry Kuzin on 21/03/2019.
+//  Copyright © 2019 Dmitry Kuzin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
