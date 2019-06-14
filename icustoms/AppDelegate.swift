84d91267d9853e15115b531323167b8db24bb7d5
eratart <contact.dbtalks@gmail.com>
2019-06-14 18:37:09 +0300

    build 10

--- icustoms/AppDelegate.swift
+++ icustoms/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             window?.rootViewController = Storyboard.Authorization.initialViewController
         }
         
-        Localization.current()
+        //Localization.current()
         
         return true
     }
