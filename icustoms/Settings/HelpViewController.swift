7de76fa1a6962dd577dab9941aa4a195c626e365
danik.lubohinec <blueray10@mail.ru>
2019-09-11 12:08:53 +0300

    Help mail, and date for all

--- icustoms/Settings/HelpViewController.swift
+++ icustoms/Settings/HelpViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class HelpViewController: UIViewController {
     
     let phone: String = "+74996776484"
+    let email = "info@icustoms.ru"
     
     @IBAction func callAction() {
         let url = URL(string: "tel://" + phone)!
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
     
+    @IBAction func emailAction(_ sender: Any) {
+        let url = URL(string: "mailto:\(email)")!
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
 }
